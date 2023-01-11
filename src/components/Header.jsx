b5822e024c8e93737e27f595738cc6c3cff04c66
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2023-01-11 23:49:50 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TfiAgenda } from "react-icons/tfi";
+
+function Header() {
+  return (
+    <header>
+      <h1>Keeper <TfiAgenda /></h1>
+    </header>
+  );
+}
+
+export default Header;
